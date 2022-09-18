652d734b8b40c0ed743e6ea98344d187ab02be08
imtiazsamir1 <imtiazsamir416@gmail.com>
2022-09-18 21:02:06 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#3b82f6",
+
+          secondary: "#4f46e5",
+
+          neutral: "#242A55",
+
+          "text-main-color": "#333333",
+
+          "text-secondary-color": "#555555",
+
+          "base-100": "#FFFFFF",
+
+          "bottom-footer": "#411361",
+
+          "main-footer": "#4D0C7A",
+
+          contact: "#952DDE",
+
+          border: "#ed3ab26b",
+
+          error: "#fc0303",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
